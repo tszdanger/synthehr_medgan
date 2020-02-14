e9272b8c6642abc0059a97c9544fc6d23542a902
anuragdutt <anurag2709@gmail.com>
2020-02-14 04:05:56 -0500

    Using Sina's train test split

---
+++ src/cerner_train_test.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import _pickle as pickle
+
+if __name__ == "__main__":
+	train_file = "../raw/x_train_v3.npy"
+	test_file = "../raw/x_test_v3.npy"
+
+	train = np.load(train_file, allow_pickle = True)
+	test = np.load(test_file, allow_pickle = True)
+
+	pickle.dump(train, open("../data/cerner_train.matrix", 'wb'), -1)
+	pickle.dump(test, open("../data/cerner_test.matrix", 'wb'), -1)
