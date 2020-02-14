e0508dc7bcbcc4736969216eabd91358efe9adf9
anuragdutt <anurag2709@gmail.com>
2020-02-13 21:15:42 -0500

    Minor changes

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -81,28 +81,29 @@ def logisticRegressionClassification(train_mat, test_mat, headers, binary = Fals
 if __name__ == "__main__":
 
 	model = sys.argv[1]
+	dataset = sys.argv[2]
 
 	headers_dict = np.load("../data/mimic_binary.types", allow_pickle = True)
 	bh = list(headers_dict.keys())
 
-	filename_generated = os.path.join("../results", model, "binary/outputs/generated.npy") 
+	filename_generated = os.path.join("..", dataset, "results", model, "binary/outputs/generated.npy") 
 	file_generated = np.load(filename_generated)
 	print(file_generated.shape)
 		
-	filename_test = os.path.join("../results", model, "binary/sumstats/valid.npy")
+	filename_test = os.path.join("..", dataset, "results", model, "binary/sumstats/valid.npy")
 	file_test = np.load(filename_test, allow_pickle = True)
 	print(file_test.shape)
 
-	filename_original = os.path.join("../results", model, "binary/sumstats/train.npy")
+	filename_original = os.path.join("..", dataset, "results", model, "binary/sumstats/train.npy")
 	file_original = np.load(filename_original, allow_pickle = True)
 	print(file_original.shape)
 
 	df = logisticRegressionClassification(train_mat = file_generated, test_mat = file_test, headers = bh, binary = True)
-	df.to_csv(os.path.join("../results", model, "binary/sumstats/logistic_regression_metrics_mimic_generated.csv"), index = False)
+	df.to_csv(os.path.join("..", dataset, "results", model, "binary/sumstats/logistic_regression_metrics_mimic_generated.csv"), index = False)
 
 
 	df = logisticRegressionClassification(train_mat = file_original, test_mat = file_test, headers = bh, binary = True)
-	df.to_csv(os.path.join("../results", model, "binary/sumstats/logistic_regression_metrics_mimic_original.csv"), index = False)
+	df.to_csv(os.path.join("..", dataset, "results", model, "binary/sumstats/logistic_regression_metrics_mimic_original.csv"), index = False)
 
 
 	# df = randomForestUndersampling(train_mat = file_generated, test_mat = file_test, headers = bh, binary = True)
