1b5f8f9adcb404cd0a566ff4041de1e6ab293b83
anuragdutt <anurag2709@gmail.com>
2020-02-14 22:12:39 -0500

    adding function to generate raw dataset not aggregated by patient id

--- src/aggregate_by_patient.py
+++ src/aggregate_by_patient.py
@@ -6,17 +6,22 @@ import pandas as pd
 
 if __name__ == "__main__":
 
+	aggregate = sys.argv[1]
+
 	adm_file = "../raw/ADMISSIONS.csv"
 	adm = pd.read_csv(adm_file)
 	adm = adm.drop(['ROW_ID'], axis = 1)
-	adm['HADM_ID'] = adm['SUBJECT_ID']
-	adm.to_csv("../raw/ADMISSIONS_aggregated.csv", index = False)
-
+	if aggregate:
+		adm['HADM_ID'] = adm['SUBJECT_ID']
+		adm.to_csv("../raw/ADMISSIONS_aggregated.csv", index = False)
+	else:
+		adm.to_csv("../raw/ADMISSIONS_not_aggregated.csv", index = False)
 
 	diag_file = "../raw/DIAGNOSES_ICD.csv"
 	diag = pd.read_csv(diag_file)
 	diag = diag.drop(['ROW_ID'], axis = 1)
-	diag['HADM_ID'] = diag['SUBJECT_ID']
-	diag['HADM_ID'] = diag['SUBJECT_ID']
-	diag.to_csv("../raw/DIAGNOSES_ICD_aggregated.csv", index = False)
-
+	if aggregate:
+		diag['HADM_ID'] = diag['SUBJECT_ID']
+		diag.to_csv("../raw/DIAGNOSES_ICD_aggregated.csv", index = False)
+	else:
+		diag.to_csv("../raw/DIAGNOSES_ICD_not_aggregated.csv", index = False)
